b95d76e4f7800f681551afb24a327144bd6ebc3a
Fredys Fernandez <juniorfrey2990gmail.com>
2021-12-13 13:11:48 -0500

    Se completo el Tutorial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+import React, { useState, useEffect } from 'react'
+import Quote from './components/Quote';
+import Spinner from './components/Spinner';
 
+
+const initialQuote = {
+    text: 'Quote',
+    author:'Autor'
+}
 
 function App() {
+
+  const [quote, setQuote] = useState(initialQuote);
+  const [loading, setloading] = useState(true);
+
+  const updateQuote = async() => {
+    setloading(true);
+    const url = `https://www.breakingbadapi.com/api/quote/random`;
+    const res = await fetch(url);
+    const [newQuote] = await res.json();
+    
+    const { quote: text, author } = newQuote;
+
+    setQuote({
+      text,
+      author: author,
+    });
+    setloading(false);
+  }
+  useEffect( async() => {
+    updateQuote();
+  } , [])
+
   return (
-    <div>
-      <h1>App</h1>
+    <div className="app">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/7/77/Breaking_Bad_logo.svg"
+        alt="logo"
+      />
+      <button onClick={() => updateQuote()}>Get Another</button>
+      {loading ? <Spinner/> : <Quote quote={quote} />}
     </div>
   );
 }
